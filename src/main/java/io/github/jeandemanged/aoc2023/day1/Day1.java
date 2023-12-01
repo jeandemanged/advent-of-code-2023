be3d36bba46436b084711cf989c918034f7e0a3b
Damien Jeandemange <damien.jeandemange@artelys.com>
2023-12-01 10:55:06 +0100

    day1 part 2

--- src/main/java/io/github/jeandemanged/aoc2023/day1/Day1.java
+++ src/main/java/io/github/jeandemanged/aoc2023/day1/Day1.java
@@ -5,38 +5,73 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day1 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Day1.class);
 
+    private static final Map<String, Integer> numbers = getNumbers();
+
+    private static Map<String, Integer> getNumbers() {
+        Map<String, Integer> numbers = new HashMap<>();
+        for (int i = 1; i <= 9; i++) {
+            numbers.put(String.valueOf(i), i);
+        }
+        numbers.put("one", 1);
+        numbers.put("two", 2);
+        numbers.put("three", 3);
+        numbers.put("four", 4);
+        numbers.put("five", 5);
+        numbers.put("six", 6);
+        numbers.put("seven", 7);
+        numbers.put("eight", 8);
+        numbers.put("nine", 9);
+        return numbers;
+    }
+
+    private static int getFirstNumber(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            Integer number = getNumber(line, i);
+            if (number != null) {
+                return number;
+            }
+        }
+        throw new IllegalArgumentException("No first number in '" + line + "'");
+    }
+
+    private static int getLastNumber(String line) {
+        for (int i = line.length() - 1; i >= 0; i--) {
+            Integer number = getNumber(line, i);
+            if (number != null) {
+                return number;
+            }
+        }
+        throw new IllegalArgumentException("No last number in '" + line + "'");
+    }
+
+    private static Integer getNumber(String line, int pos) {
+        for (var entry : numbers.entrySet()) {
+            var key = entry.getKey();
+            var value = entry.getValue();
+            if (line.startsWith(key, pos)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         var lines = FileUtils.readAllLines(Paths.get("data", "day1.txt"));
         int sum = 0;
         for (String line : lines) {
-            Character first = null;
-            Character last = null;
-            for (int i = 0; i < line.length(); i++) {
-                var c = line.charAt(i);
-                if (Character.isDigit(c)) {
-                    if (first == null) {
-                        first = c;
-                        last = c;
-                    } else {
-                        last = c;
-                    }
-                }
-            }
-            if (first == null) {
-                LOGGER.error("Invalid line '{}'", line);
-                continue;
-            }
-            char[] chars = {first, last};
-            String lineValueString = new String(chars);
-            int lineValue = Integer.parseInt(lineValueString);
+            int first = getFirstNumber(line);
+            int last = getLastNumber(line);
+            int lineValue = Integer.parseInt(String.valueOf(first) + last);
             sum += lineValue;
-            LOGGER.info("'{}' -> first={} last={} value={}", line, first, last, lineValue);
+            LOGGER.info("  '{}' -> first={} last={} value={}", line, first, last, lineValue);
         }
-        LOGGER.info("Day1: {}", sum);
+        LOGGER.info("Day1 Part 2: {}", sum);
     }
 }
