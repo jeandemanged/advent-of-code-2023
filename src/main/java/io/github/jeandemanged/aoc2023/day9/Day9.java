8704f4d1c43c7521b4f072e0ed436ca536e44083
Damien Jeandemange <damien.jeandemange@artelys.com>
2023-12-09 12:33:45 +0100

    day9 part1

---
+++ src/main/java/io/github/jeandemanged/aoc2023/day9/Day9.java
@@ -0,0 +1,73 @@
+package io.github.jeandemanged.aoc2023.day9;
+
+import io.github.jeandemanged.aoc2023.utils.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day9 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day9.class);
+
+    record History(List<Long> history) {
+        long predictNext() {
+            List<List<Long>> sequences = new ArrayList<>();
+            // down
+            List<Long> current = new ArrayList<>(this.history());
+            while (!allZeros(current)) {
+                sequences.add(current);
+                current = toNext(current);
+            }
+            sequences.add(current);
+            // up
+            for (int i = sequences.size() - 1; i >= 0; i--) {
+                List<Long> seq = sequences.get(i);
+                if (i == sequences.size() - 1) {
+                    seq.add(0L);
+                } else {
+                    long lastVal = seq.get(seq.size() - 1);
+                    List<Long> dnSeq = sequences.get(i + 1);
+                    long lastDiff = dnSeq.get(dnSeq.size() - 1);
+                    seq.add(lastVal + lastDiff);
+                }
+            }
+            List<Long> first = sequences.get(0);
+            Long predicted = first.get(first.size() - 1);
+            LOGGER.info("Predicted {} (depth={})", predicted, sequences.size());
+            return predicted;
+        }
+    }
+
+    record Puzzle(List<History> historyList) {
+        static Puzzle build(List<String> lines) {
+            var puzzle = new Puzzle(new ArrayList<>());
+            lines.forEach(line -> puzzle.historyList().add(new History(Arrays.stream(line.split(" ")).map(Long::parseLong).toList())));
+            return puzzle;
+        }
+    }
+
+    static List<Long> toNext(List<Long> list) {
+        List<Long> newList = new ArrayList<>(list.size() - 1);
+        for (int i = 0; i < list.size() - 1; i++) {
+            newList.add(list.get(i + 1) - list.get(i));
+        }
+        return newList;
+    }
+
+    static boolean allZeros(List<Long> list) {
+        return list.stream().allMatch(l -> 0L == l);
+    }
+
+
+    public static void main(String[] args) {
+        var puzzle = Puzzle.build(FileUtils.readAllLines(Paths.get("data", "day9.txt")));
+
+        long part1 = puzzle.historyList().stream().mapToLong(History::predictNext).sum();
+        LOGGER.info("Part 1: {}", part1);
+    }
+
+}
